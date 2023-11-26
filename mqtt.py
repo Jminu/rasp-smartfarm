9060bc66b5898760874390f031d47f2e64dd4b07
Jin Minu <s9430939@naver.com>
2023-11-26 23:05:35 +0900

    버튼 수정

--- mqtt.py
+++ mqtt.py
@@ -17,7 +17,6 @@ client.on_message = on_message
 client.connect(ip, 1883) # 브로커에 연결
 client.loop_start() # 메시지 루프를 실행하는 스레드 생성
 
-# 도착하는 메시지는 on_message() 함수에 의해 처리되어 LED를 켜거나 끄는 작업과
 # 병렬적으로 1초 단위로 초음파 센서로부터 거리를 읽어 전송하는 무한 루프 실행
 while True:
 	temp = temp_humid.getTemperature(temp_humid.sensor) #온도 읽기
