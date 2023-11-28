53c7216674f0ba8d400ba51fd1756e26aee784b0
Jin Minu <s9430939@naver.com>
2023-11-28 17:05:51 +0900

    app.py수정 : 캐시 사용 x

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+app.config['SEND_FILE_MAX_AGE_DEFAULT']=0
+
 @app.route('/')
 def index():
         return render_template('mainWeb.html')
