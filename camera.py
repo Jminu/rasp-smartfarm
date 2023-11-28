3332ba434ff8aa6807fa06c65631c9f6f2cd8d55
Jin Minu <s9430939@naver.com>
2023-11-28 23:48:10 +0900

    사진 저장경로 다시 수정

--- camera.py
+++ camera.py
@@ -8,13 +8,13 @@ def init_camera():
     camera = cv2.VideoCapture(0, cv2.CAP_V4L)
 
 def shot_camera(count):
-    image_folder = '/home/pi/smartplanter/rasp-smartfarm/image'
+    image_folder = '/home/pi/smartplanter/rasp-smartfarm/image/'
     image_name = f'image_{count}.jpg'
     image_path = image_folder + image_name
 
     ret, image = camera.read()
     if(ret == True):
-        cv2.imwrite(image_folder, image) # OpenCV 함수로 이미지를 파일에 저장
+        cv2.imwrite(image_path, image) # OpenCV 함수로 이미지를 파일에 저장
     else:
         print("camera error!")
 
