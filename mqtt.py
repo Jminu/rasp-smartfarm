e3caee07df19d6a50da2627b721285c33f181cc3
Jin Minu <s9430939@naver.com>
2023-11-29 20:14:04 +0900

    수정

--- mqtt.py
+++ mqtt.py
@@ -13,9 +13,9 @@ def on_connect(client, userdata, flag, rc): #브로커에 연결시
 	client.subscribe("led", qos = 0) #led구독 신청
 def on_message(client, userdata, msg) : #브로커 연결되고 
 	print("on_message connected to broker")
-	if msg.payload == 1:
+	if int(msg.payload) == 1:
 		waterpump.watering(13, 19, 26)
-	else:
+	elif int(msg.payload) == 0:
 		waterpump.watering_stop(13, 19, 26)
 
 ip = "localhost" # 현재 브로커는 이 컴퓨터에 설치되어 있음
